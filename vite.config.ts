f12ce0ab9903231683e069699e1de77cdc11ab88
zanndev0511 <giangtruong051103@gmail.com>
2025-10-11 16:19:50 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  base: '/',
   plugins: [
-    react(), tailwindcss()
+    react(), tailwindcss(), vercel()
   ],
   
 
